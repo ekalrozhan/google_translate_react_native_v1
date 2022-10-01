f9e00b4e39506c3bba3aa1df32a11b5dae5ee4eb
haikalrozhan <haikalrozhan1992@yahoo.com>
2022-10-01 14:13:28 +0800

    setup stack navigator

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { StyleSheet, View } from "react-native";
+
+import HomeScreen from "./screens/HomeScreen";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={{ flex: 1 }}>
+        <Stack.Navigator>
+          <Stack.Group>
+            <Stack.Screen
+              name="main"
+              component={HomeScreen}
+              options={{
+                headerTitle: "Translate",
+                headerTitleAlign: "center",
+              }}
+            />
+          </Stack.Group>
+        </Stack.Navigator>
+      </View>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
